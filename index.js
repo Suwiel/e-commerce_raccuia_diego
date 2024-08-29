dc3362fba43d9ecf771b0aaccb00cccd1478bb40
Suwiel <diegz12343@gmail.com>
2024-08-29 20:48:53 -0300

    3 cards

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("hola");
+const h5 = document.querySelectorAll("h5");
+const text = document.querySelectorAll(".card-text");
+const imagen = document.querySelectorAll(".card img");
+
+h5.forEach((h5) => {
+    h5.innerText = "Hola";
+});
+
+text.forEach((text) => {
+    text.innerText = "holahola";
+});
+
+imagen.forEach((imagen) =>{
+    imagen.src = "https://media.istockphoto.com/id/1334993248/es/vector/rana-verde-sobre-fondo-blanco.jpg?s=612x612&w=0&k=20&c=37vajhZ1LjihBvZ21nHixOb2n9hlWv7RhTYIHY1GU1w="
+})
