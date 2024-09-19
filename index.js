d1c3e01ab766c451467ad57adec168160ad6b663
Suwiel <diegz12343@gmail.com>
2024-09-19 20:22:21 -0300

    productos

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ for (let i = 1; i < 10; i++) {
   <div class="card-body">
     <h5 class="card-title">Card ${1}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Ver más</a>
+    <a href="producto.html" class="btn btn-primary">Ver más</a>
   </div>
 </div>`)
   section.innerHTML = array.join().replaceAll(",", "");
---
+++ producto.js
@@ -0,0 +1,24 @@
+class Producto {
+    constructor(titulo, detalle, precio, stock , imagen){
+    this.titulo = titulo;
+    this.detalle = detalle;
+    this.precio = precio;
+    this.stock = stock;
+    this.imagen = imagen;
+}
+}
+
+const auto = new Producto("auto" , "fachero ", "100$", 120, "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg");
+
+let etiquetas = `<div>
+<h1> Hola </h1>
+<div> Titulo ${auto.titulo}</div>
+<div> Detalle ${auto.detalle}</div>
+<div> Precio ${auto.precio}</div>
+<div> Stock ${auto.stock}</div>
+<div> Imagen <img src="${auto.imagen}" alt="Imagen del auto widht="150" height="150" /></div>
+</div>
+`
+
+let main = document.querySelector("main");
+main.innerHTML = etiquetas;
